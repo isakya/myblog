1bbf0c073d5f29f5253efb085f5a592fd3df5e0a
konglong <1016361268@qq.com>
2022-06-17 11:43:07 +0800

    初始化项目

--- src/main.js
+++ src/main.js
@@ -4,6 +4,13 @@ import router from './router'
 
 Vue.config.productionTip = false
 
+import 'muse-ui/lib/styles/base.less'
+import { Button, Select } from 'muse-ui'
+import 'muse-ui/lib/styles/theme.less'
+Vue.use(Button)
+Vue.use(Select)
+
+
 new Vue({
   router,
   render: h => h(App)
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,7 +1,13 @@
 <template>
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <HelloWorld msg="Welcome to Your Vue.js App" />
+    <mu-button color="primary">Primary</mu-button>
+    <mu-button color="secondary">Secondary</mu-button>
+    <mu-button color="success">Success</mu-button>
+    <mu-button color="warning">Warning</mu-button>
+    <mu-button color="info">Info</mu-button>
+    <mu-button color="error">Error</mu-button>
   </div>
 </template>
 
