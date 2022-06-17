b3ba5469dad009b2ef558ecef794477445a16878
konglong <1016361268@qq.com>
2022-06-17 12:18:51 +0800

    首页背景及点击样式和打字效果

--- src/main.js
+++ src/main.js
@@ -3,7 +3,8 @@ import App from './App.vue'
 import router from './router'
 
 Vue.config.productionTip = false
-
+import './global.less'
+import 'lib-flexible'
 import 'muse-ui/lib/styles/base.less'
 import { Button, Select } from 'muse-ui'
 import 'muse-ui/lib/styles/theme.less'
